95c5cce3aae53362c8ae9894a9e82f1cc012afce
Seabert Yuan <seabert@pop-os.localdomain>
2021-01-03 17:03:03 -0700

    small changes

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".navLinks");
+const links = document.querySelectorAll(".navLinks a");
+
+burger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
