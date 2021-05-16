1ad605b7cf99a1ddedc386327c9bf6595afa9e63
Seabert Yuan <seabert@pop-os.localdomain>
2021-05-16 12:17:47 -0600

    major changes asthetically

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@ burger.addEventListener("click", () => {
 
     burger.classList.toggle("toggle");
 });
+
+const quotes = [
+    "don't stop dreaming",
+    "live for yourself",
+    "create against all odds",
+    "you won't know what you have until you lose it",
+    "home-made"
+];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+const elmId = "mainSubtitle";
+const value = quotes[getRandomInt(5)];
+
+function updateHTML(elmId, value) {
+  var elem = document.getElementById(elmId);
+  if(typeof elem !== 'undefined' && elem !== null) {
+    elem.textContent = value;
+    console.log("yes");
+  }
+}
+
+$(document).ready(() => {
+    updateHTML(elmId, value);
+  }
+);
--- scripts/projects.js
+++ scripts/projects.js
@@ -4,8 +4,18 @@ function getRandomSize(min, max) {
 
 var allImages = "";
 
-for(var i = 1; i <= 5; i++){
+for(var i = 1; i <= 4; i++){
     allImages = `<img src="/resources/gallery-images/img-${i}.jpg" alt="my pictures">`;
-    $('#gallery').append(allImages);
+    $('#galleryColumn0').append(allImages);
+    console.log("image"+ i + allImages);
+}
+for(var i = 5; i <= 8; i++){
+    allImages = `<img src="/resources/gallery-images/img-${i}.jpg" alt="my pictures">`;
+    $('#galleryColumn1').append(allImages);
+    console.log("image"+ i + allImages);
+}
+for(var i = 9; i <= 11; i++){
+    allImages = `<img src="/resources/gallery-images/img-${i}.jpg" alt="my pictures">`;
+    $('#galleryColumn2').append(allImages);
     console.log("image"+ i + allImages);
 }
